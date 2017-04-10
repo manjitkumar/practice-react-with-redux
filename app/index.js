ea6f11921714aeabfc7462d5e79426cb92e66e37
Manjit Kumar <manjit1727@gmail.com>
2017-04-10 23:50:22 +0530

    use Provider from react-redux

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import { todoAppReducer } from './scripts/reducers';
 import { TodoApp } from './scripts/components';
@@ -8,21 +9,21 @@ import { TodoApp } from './scripts/components';
 import styles from './stylesheets/main.css';
 
 
-class Provider extends React.Component {
-    getChildContext() {
-        return {
-            store: this.props.store
-        };
-    }
+// class Provider extends React.Component {
+//     getChildContext() {
+//         return {
+//             store: this.props.store
+//         };
+//     }
 
-    render() {
-        return this.props.children;
-    }
-}
+//     render() {
+//         return this.props.children;
+//     }
+// }
 
-Provider.childContextTypes = {
-    store: React.PropTypes.object
-}
+// Provider.childContextTypes = {
+//     store: React.PropTypes.object
+// }
 
 
 ReactDom.render(
