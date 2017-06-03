f83043cf2330f26cf6b38ef2094d11106558d35f
Manjit Kumar <manjit1727@gmail.com>
2017-06-03 23:35:52 +0530

    use inbuilt applyMiddeware and logger, promise from redux-libs

--- app/scripts/configureStore.js
+++ app/scripts/configureStore.js
@@ -1,53 +1,22 @@
 import throttle from 'lodash/throttle';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import { createLogger } from 'redux-logger';
+
 import todoAppReducer from './reducers';
 import { fetchTodos } from './apis';
 
 
-const logger = (store) => (next) => {
-    if (!console.group) {
-        return next;
-    }
-    return (action) => {
-        console.group(action.type);
-        console.log('%c prevState', 'color: grey', store.getState());
-        console.log('%c action', 'color: blue', action);
-        const returnValue = next(action);
-        console.log('%c nextState', 'color: green', store.getState());
-        console.groupEnd(action.type);
-        return returnValue;
-    };
-};
-
-
-const promise = (store) => (next) => {
-    return (action) => {
-        if (typeof action.then === 'function') {
-            return action.then(next);
-        }
-        return next(action);
-    };
-};
-
-const applyMiddlewaresToStore = (store, middlewares) => {
-    middlewares.slice.reverse().forEach((middleware) =>
-        store.dispatch = middleware(store)(store.dispatch)
-    );
-};
-
 const configureStore = () => {
 
+    const middlewares = [promise];
+    middlewares.push(createLogger());
     // create store with root reducer and persisted state
     // from local storage
-    const store = createStore(
+    return createStore(
         todoAppReducer,
+        applyMiddleware(...middlewares)
     );
-
-    const middlewares = [promise];
-    middlewares.push(logger);
-    store = applyMiddlewaresToStore(store, middlewares);
-
-    return store;
 }
 
 export default configureStore;
